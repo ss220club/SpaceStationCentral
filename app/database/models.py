635cff2b677e8689563b5e443ad956a39203bd3b
Furior <furiorg@gmail.com>
2024-12-23 06:06:09 +0700

    commit, push, leave the building

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,32 @@
+from datetime import datetime, timedelta
+from secrets import token_urlsafe
 from sqlmodel import Field, SQLModel
 
 class Player(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    discord_id: str = Field(unique=True)
-    ckey: str = Field(unique=True)
+    discord_id: str = Field(unique=True, index=True)
+    ckey: str = Field(unique=True, index=True)
+
+class OneTimeToken(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    ckey: str = Field(unique=True, index=True)
+    token: str = Field(unique=True, default_factory=lambda: token_urlsafe(32), index=True)
+    expiry: datetime = Field(default_factory=lambda: datetime.now() + timedelta(minutes=5)) 
+
+class Whitelist(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    player_id: int = Field(foreign_key="player.id", index=True)
+    type: str
+    admin_id: int = Field(foreign_key="player.id")
+    issue_time: datetime = Field(default_factory=datetime.now)
+    duration: timedelta = Field(default=timedelta(days=30))
+    active: bool = Field(default=True)
+
+class WhitelistBan(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    player_id: int = Field(foreign_key="player.id", index=True)
+    type: str
+    admin_id: int = Field(foreign_key="player.id")
+    issue_time: datetime = Field(default_factory=datetime.now)
+    duration: timedelta = Field(default=timedelta(days=14))
+    active: bool = Field(default=True)
--- app/routes/player.py
+++ app/routes/player.py
@@ -1,15 +1,15 @@
+import datetime
 import logging
 logger = logging.getLogger("main-logger")
 
-from hashlib import md5
 from fastapi import APIRouter, HTTPException
 from fastapi import status
 from fastapi.responses import RedirectResponse
-from sqlmodel import select
+from sqlmodel import Session, select
 from app.fur_discord import DiscordOAuthClient
 
 from app.core.config import Config
-from app.database.models import Player
+from app.database.models import OneTimeToken, Player
 from app.deps import SessionDep
 
 router = APIRouter(prefix="/player", tags=["Player"])
@@ -19,29 +19,70 @@ oauth_client = DiscordOAuthClient(
     Config.Oauth.CLIENT_ID, Config.Oauth.CLEINT_SECRET, f"{Config.General.ENDPOINT_URL}{router.prefix}{CALLBACK_PATH}"
 )
 
-@router.get("/login")
-async def login(ckey: str, token: str):
-    return RedirectResponse(oauth_client.get_oauth_login_url(f"{ckey}+{token}"))
+@router.get("/login", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
+async def login(token: str) -> RedirectResponse:
+    """
+    Redirects to the discord oauth2 login page with the given ckey and state token
+    """
+    return RedirectResponse(oauth_client.get_oauth_login_url(token))
 
-def generate_token(ckey: str):
-    token = md5((ckey + Config.Oauth.STATE_SECRET).encode()).hexdigest()
-    return token
+def get_token_by_ckey(session: Session, ckey: str) -> str:
+    token_entry = session.exec(select(OneTimeToken).where(OneTimeToken.ckey == ckey)).first()
+    if token_entry is None:
+        token_entry = OneTimeToken(ckey=ckey)
+    elif token_entry.expiry < datetime.datetime.now():
+        session.delete(token_entry)
+        session.commit()
+        token_entry = OneTimeToken(ckey=ckey)
+    else:
+        return token_entry.token
 
-def is_state_valid(ckey: str, token: str):
-    return generate_token(ckey) == token
+    session.add(token_entry)
+    session.commit()
+    session.refresh(token_entry)
 
-@router.get("/login/g/{ckey}")
-async def generate_state(ckey: str):
-    token = generate_token(ckey)
+    return token_entry.token
+
+def get_token_owner(session: Session, token: str) -> str:
+    """
+    Assumes that the token is valid
+    """
+    return session.exec(select(OneTimeToken).where(OneTimeToken.token == token)).first().ckey
+
+def is_state_valid(session: Session, token: str):
+    """
+    Checks if the given state token is valid for the given ckey.
+    """
+    token_entry = session.exec(select(OneTimeToken).where(OneTimeToken.token == token and OneTimeToken.expiry > datetime.datetime.now())).first()
+    if token_entry is None:
+        return False
+    return True
+
+@router.get("/token/{ckey}")
+async def generate_state(session: SessionDep, ckey: str):
+    """
+    Generates a state token for the given ckey and returns it. The state token
+    is used to validate the authorization flow.
+    """
+    token = get_token_by_ckey(session, ckey)
     return token
 
 @router.get(CALLBACK_PATH)
-async def callback(session: SessionDep, code: str, state: str):
+async def callback(session: SessionDep, code: str, state: str) -> Player:
+    """
+    The callback endpoint for the discord oauth2 flow. It takes the code and state parameters
+    and verifies the state token. If the state token is invalid, it raises a 401 Unauthorized
+    response. If the state token is valid, it uses the code to get an access token for the
+    discord user. It then checks if the player is already linked with the discord user. If
+    the player is already linked, it raises a 409 Conflict response. If the player is not
+    already linked, it creates a new Player object with the ckey and discord_id and adds it
+    to the database. It then returns the newly created Player object.
+    """
     discord_token, _ = await oauth_client.get_access_token(code)
-    ckey, token = state.split(" ")
-    if not is_state_valid(ckey, token):
-        logger.debug("Got invalid auth state: %s", state)
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Spoofed or invalid state")
+    token = state
+    if not is_state_valid(session, token):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong or expired token")
+    ckey = get_token_owner(session, token)
     user = await oauth_client.get_user(discord_token)
     discord_id = user.id
 
@@ -61,6 +102,10 @@ async def callback(session: SessionDep, code: str, state: str):
 
 @router.get("/ckey/{ckey}")
 async def get_player_by_ckey(session: SessionDep, ckey: str):
+    """
+    Retrieves a player by their ckey.
+    """
+
     player = session.exec(select(Player).where(Player.ckey == ckey)).first()
     if player is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
@@ -68,6 +113,9 @@ async def get_player_by_ckey(session: SessionDep, ckey: str):
 
 @router.get("/discord/{discord_id}")
 async def get_player_by_discord(session: SessionDep, discord_id: str):
+    """
+    Retrieves a player by their discord ID.
+    """
     player = session.exec(select(Player).where(Player.discord_id == discord_id)).first()
     if player is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
