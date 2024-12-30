042c3ea89113fe5611655f057cf2a8941de1c476
Furior <furiorg@gmail.com>
2024-12-30 09:04:32 +0700

    wip

--- app/routes/whitelist.py
+++ app/routes/whitelist.py
@@ -1,11 +1,11 @@
+import datetime
 import logging
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import select
 
-from app.database.models import Player, Whitelist
+from app.database.models import Player, Whitelist, WhitelistBan
 from app.deps import SessionDep, verify_bearer
-from app.schemas.whitelist import NewWhitelist
 
 logger = logging.getLogger("main-logger")
 
@@ -14,8 +14,17 @@ router = APIRouter(prefix="/whitelist", tags=["Whitelist"])
 
 
 @router.post("/", dependencies=[Depends(verify_bearer)], status_code=status.HTTP_201_CREATED)
-def create_whitelist(session: SessionDep, new_wl: NewWhitelist) -> Whitelist:
-    wl = Whitelist.model_validate(new_wl)
+def create_whitelist(session: SessionDep, wl: Whitelist, ignore_bans: bool = False) -> Whitelist:
+    if not ignore_bans:
+        bans = session.exec(select(WhitelistBan).where(
+            WhitelistBan.player_id == wl.player_id
+            and WhitelistBan.valid == True
+            and WhitelistBan.type == wl.type
+            and WhitelistBan.issue_time + WhitelistBan.duration < wl.issue_time)).first()
+        if bans is not None:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Player is banned",
+                            headers={"X-Retry-After": str(WhitelistBan.issue_time + WhitelistBan.duration)})
+
     session.add(wl)
     session.commit()
     session.refresh(wl)
@@ -26,4 +35,32 @@ def create_whitelist(session: SessionDep, new_wl: NewWhitelist) -> Whitelist:
 async def get_whitelist_by_ckey(session: SessionDep, ckey: str, valid: bool = True) -> list[Whitelist]:
     result = session.exec(select(Whitelist).join(Player, onclause=Player.discord_id == Whitelist.player_id)
                           .where(Player.ckey == ckey and Whitelist.valid == valid))
+    return result.all()
+
+@router.get("/discord/{discord_id}", status_code=status.HTTP_200_OK)
+async def get_whitelist_by_discord(session: SessionDep, discord_id: str, valid: bool = True) -> list[Whitelist]:
+    result = session.exec(select(Whitelist).join(Player, onclause=Player.discord_id == Whitelist.player_id)
+                          .where(Player.discord_id == discord_id and Whitelist.valid == valid))
+    return result.all()
+
+@router.post("/ban", dependencies=[Depends(verify_bearer)], status_code=status.HTTP_201_CREATED)
+def ban_whitelist(session: SessionDep, wl: Whitelist, invalidate_old_wls: bool = True) -> WhitelistBan:
+    ban = WhitelistBan(player_id=wl.player_id, type=wl.type, admin_id=wl.admin_id, duration=wl.duration)
+    session.add(ban)
+    if invalidate_old_wls:
+        old_wls = session.exec(select(Whitelist).where(Whitelist.player_id == wl.player_id)).all()
+        for old_wl in old_wls:
+            old_wl.valid = False
+        session.add_all(old_wls)
+
+    session.commit()
+    session.refresh(wl)
+    return wl
+
+@router.get("/ban/discord/{discord_id}", status_code=status.HTTP_200_OK)
+async def get_whitelist_bans_by_discord(session: SessionDep, discord_id: str, only_active: bool = True) -> list[WhitelistBan]:
+    result = session.exec(select(WhitelistBan).where(
+        WhitelistBan.player_id == discord_id
+        and WhitelistBan.valid == only_active
+        and WhitelistBan.issue_time + WhitelistBan.duration > datetime.now()))
     return result.all()
--- app/tests/test_whitelist_route.py
+++ app/tests/test_whitelist_route.py
@@ -22,6 +22,11 @@ def test_get_whitelists(client, db_session, discord_id, discord_id2, ckey, ckey2
     wl_result = Whitelist.model_validate(response.json()[0])
     assert wl_result == wl
 
+    response = client.get(f"/whitelist/discord/{discord_id}")
+    assert response.status_code == 200
+    wl_result = Whitelist.model_validate(response.json()[0])
+    assert wl_result == wl
+
 
 def test_post_whitelist(client, db_session, bearer, discord_id, discord_id2, ckey, ckey2, duration_days):
     player = Player(ckey=ckey, discord_id=discord_id)
@@ -35,3 +40,20 @@ def test_post_whitelist(client, db_session, bearer, discord_id, discord_id2, cke
     response = client.post("/whitelist", json=wl.model_dump(mode="json"),
                            headers={"Authorization": f"Bearer {bearer}"})
     assert response.status_code == 201
+
+def test_post_whitelist_ban(client, db_session, bearer, discord_id, discord_id2, ckey, ckey2, duration_days):
+    player = Player(ckey=ckey, discord_id=discord_id)
+    admin = Player(ckey=ckey2, discord_id=discord_id2)
+    db_session.add_all([player, admin])
+    db_session.commit()
+
+    wl = Whitelist(player_id=discord_id, type="test",
+                   admin_id=discord_id2, duration=duration_days)
+
+    response = client.post("/whitelist", json=wl.model_dump(mode="json"),
+                           headers={"Authorization": f"Bearer {bearer}"})
+    assert response.status_code == 201
+
+
+
+    
