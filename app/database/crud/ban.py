ed28dabedf8dba95458b6c7924f4b6f3f00ce83f
Furior <furiorg@gmail.com>
2025-04-25 15:36:16 +0700

    async crud because v2 should use async session

--- app/database/crud/ban.py
+++ app/database/crud/ban.py
@@ -1,6 +1,6 @@
 import logging
 
-from sqlmodel import Session
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.database.models import Ban, BanHistory, BanHistoryAction
 from app.schemas.v2.ban import BanUpdate
@@ -9,29 +9,29 @@ from app.schemas.v2.ban import BanUpdate
 logger = logging.getLogger(__name__)
 
 
-def create_ban(db: Session, ban: Ban) -> Ban:
+async def create_ban(db: AsyncSession, ban: Ban) -> Ban:
     db.add(ban)
-    db.commit()
+    await db.commit()
     logger.info("Created ban: %s", ban)
-    db.refresh(ban)
+    await db.refresh(ban)
     history = BanHistory(ban_id=ban.id, admin_id=ban.admin_id, action=BanHistoryAction.CREATE, details=ban.reason)  # pyright: ignore[reportArgumentType]
     db.add(history)
-    db.commit()
+    await db.commit()
     logger.debug("Created ban history: %s", history)
     return ban
 
 
-def update_ban(db: Session, ban_id: int, update: BanUpdate) -> Ban:
-    ban = db.get(Ban, ban_id)
+async def update_ban(db: AsyncSession, ban_id: int, update: BanUpdate) -> Ban:
+    ban = await db.get(Ban, ban_id)
     if ban is None:
         raise ValueError("Ban not found")
     update_data = update.model_dump(exclude_unset=True)
     for key, value in update_data.items():
         setattr(ban, key, value)
     db.add(ban)
-    db.commit()
+    await db.commit()
     logger.info("Updated ban: %s", ban)
-    db.refresh(ban)
+    await db.refresh(ban)
     history = BanHistory(
         ban_id=ban.id,  # pyright: ignore[reportArgumentType]
         admin_id=ban.admin_id,
@@ -39,6 +39,6 @@ def update_ban(db: Session, ban_id: int, update: BanUpdate) -> Ban:
         details=update.model_dump_json(),
     )
     db.add(history)
-    db.commit()
+    await db.commit()
     logger.debug("Created ban history: %s", history)
     return ban
