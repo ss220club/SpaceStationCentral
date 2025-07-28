bb643117b0b15856327db45dca2ed1392cb080a4
Furior <furiorg@gmail.com>
2025-07-28 22:33:57 +0700

    Separately test metadata init and create player

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -1,18 +1,25 @@
-from app.database.models import Player
 from sqlmodel import Session
+
+from tests.conftest import create_player
+
+
+class TestDBMetadata:
+    def test_init_db(self, db_session: Session) -> None:
+        assert db_session
 
 
 class TestCreateEntry:
+    """
+    Test that models and relations are correct by creating instances.
+    """
+
     def test_create_player(
         self,
         db_session: Session,
         discord_id: str,
         ckey: str,
     ) -> None:
-        player = Player(discord_id=discord_id, ckey=ckey)
-        db_session.add(player)
-        db_session.commit()
-        db_session.refresh(player)
+        player = create_player(db_session, ckey, discord_id)
 
         assert player.discord_id == discord_id
         assert player.ckey == ckey
